c19528efb54db152e25658ab8fd07ef1a2a4d841
Steve Cherry <stevenccherry@gmail.com>
2016-07-06 19:45:57 -0400

    Count up from wedding day

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@
 
   // Custom functions
   function countdown() {
-    var $el = $('[data-countdown]'),
-      then = new Date($($el.data('countdown')).attr('datetime')).getTime();
+    var $el = $('[data-bind="countdown"]'),
+      then = new Date($el.data('datetime')).getTime();
 
     setInterval(function () {
       var now = new Date().getTime(),
         days, hourse, minutes, seconds, countdown;
 
-      seconds = (then - now) / 1000;
+      seconds = (now - then) / 1000;
       days = parseInt(seconds / 86400);
       seconds = seconds % 86400;
       hours = parseInt(seconds / 3600);
